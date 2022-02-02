47e1b1258c2084f029826f5be90d1c5081b924e3
Reid Sanders <reid@reidsanders.net>
2022-02-02 13:00:49 -0500

    tpu_setup.sh install tmux fix

---
+++ scripts/tpu_setup.sh
@@ -0,0 +1,13 @@
+## conda install
+#curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o Miniconda3-latest-Linux-x86_64.sh
+#bash Miniconda3-latest-Linux-x86_64.sh
+
+
+#git clone https://github.com/samoshkin/tmux-config.giit
+git clone --branch patch-1 https://github.com/computertoucher/tmux-config.git
+./tmux-config/install.sh
+
+cp ./.inputrc ~/
+
+export XRT_TPU_CONFIG="localservice;0;localhost:51011"
+
