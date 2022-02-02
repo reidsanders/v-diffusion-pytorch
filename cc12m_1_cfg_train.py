465a617e83bc9bd771f08dbe83ad708d70feb171
Reid Sanders <reid@reidsanders.net>
2022-02-02 16:51:39 -0500

    remove flush_logs_every_n again

--- cc12m_1_cfg_train.py
+++ cc12m_1_cfg_train.py
@@ -690,7 +690,7 @@ def main():
         logger=wandb_logger,
         log_every_n_steps=100,
         max_epochs=2,
-        flush_logs_every_n_steps=100,
+        #flush_logs_every_n_steps=100,
         #resume_from_checkpoint=args.checkpoint,
     )
     #trainer.fit(model, train_dl, ckpt_path=args.checkpoint)
