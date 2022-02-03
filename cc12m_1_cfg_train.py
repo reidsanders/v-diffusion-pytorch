26f94b8e1bc8dea7cea5288e76d69ffcd42207d8
Reid Sanders <reid@reidsanders.net>
2022-02-02 19:13:55 -0500

    comment out in sample

--- cc12m_1_cfg_train.py
+++ cc12m_1_cfg_train.py
@@ -343,48 +343,50 @@ def sample(model, x, steps, eta, extra_args, guidance_scale=1.):
     # Create the noise schedule
     t = torch.linspace(1, 0, steps + 1)[:-1]
     alphas, sigmas = get_alphas_sigmas(t)
+    return alphas
+    
+    ### TODO temp commented out
+    ## The sampling loop
+    #for i in trange(steps):
 
-    # The sampling loop
-    for i in trange(steps):
+        ## Get the model output (v, the predicted velocity)
+        ##with torch.cuda.amp.autocast():
+        ### NOTE removed above cuda line with no changes...
+        #x_in = torch.cat([x, x])
+        #ts_in = torch.cat([ts, ts])
+        #clip_embed = extra_args['clip_embed']
+        #clip_embed = torch.cat([clip_embed, torch.zeros_like(clip_embed)])
+        #v_uncond, v_cond = model(
+            #x_in, ts_in * t[i], {
+                #'clip_embed': clip_embed
+            #}
+        #).float().chunk(2)
 
-        # Get the model output (v, the predicted velocity)
-        #with torch.cuda.amp.autocast():
-        ## NOTE removed above cuda line with no changes...
-        x_in = torch.cat([x, x])
-        ts_in = torch.cat([ts, ts])
-        clip_embed = extra_args['clip_embed']
-        clip_embed = torch.cat([clip_embed, torch.zeros_like(clip_embed)])
-        v_uncond, v_cond = model(
-            x_in, ts_in * t[i], {
-                'clip_embed': clip_embed
-            }
-        ).float().chunk(2)
+        #v = v_uncond + guidance_scale * (v_cond - v_uncond)
 
-        v = v_uncond + guidance_scale * (v_cond - v_uncond)
+        ## Predict the noise and the denoised image
+        #pred = x * alphas[i] - v * sigmas[i]
+        #eps = x * sigmas[i] + v * alphas[i]
 
-        # Predict the noise and the denoised image
-        pred = x * alphas[i] - v * sigmas[i]
-        eps = x * sigmas[i] + v * alphas[i]
+        ## If we are not on the last timestep, compute the noisy image for the
+        ## next timestep.
+        #if i < steps - 1:
+            ## If eta > 0, adjust the scaling factor for the predicted noise
+            ## downward according to the amount of additional noise to add
+            #ddim_sigma = eta * (sigmas[i + 1]**2 / sigmas[i]**2).sqrt() * \
+                #(1 - alphas[i]**2 / alphas[i + 1]**2).sqrt()
+            #adjusted_sigma = (sigmas[i + 1]**2 - ddim_sigma**2).sqrt()
 
-        # If we are not on the last timestep, compute the noisy image for the
-        # next timestep.
-        if i < steps - 1:
-            # If eta > 0, adjust the scaling factor for the predicted noise
-            # downward according to the amount of additional noise to add
-            ddim_sigma = eta * (sigmas[i + 1]**2 / sigmas[i]**2).sqrt() * \
-                (1 - alphas[i]**2 / alphas[i + 1]**2).sqrt()
-            adjusted_sigma = (sigmas[i + 1]**2 - ddim_sigma**2).sqrt()
+            ## Recombine the predicted noise and predicted denoised image in the
+            ## correct proportions for the next step
+            #x = pred * alphas[i + 1] + eps * adjusted_sigma
 
-            # Recombine the predicted noise and predicted denoised image in the
-            # correct proportions for the next step
-            x = pred * alphas[i + 1] + eps * adjusted_sigma
+            ## Add the correct amount of fresh noise
+            #if eta:
+                #x += torch.randn_like(x) * ddim_sigma
 
-            # Add the correct amount of fresh noise
-            if eta:
-                x += torch.randn_like(x) * ddim_sigma
-
-    # If we are on the last timestep, output the denoised image
-    return pred
+    ## If we are on the last timestep, output the denoised image
+    #return pred
 
 
 class TokenizerWrapper:
