3ba8122e8023a844eab9ed22b82f96a6ecef3e28
Reid Sanders <reid@reidsanders.net>
2022-02-20 19:01:19 -0500

    remove emoji dataset test, tpu_setup working on conda install

--- scripts/tpu_setup.sh
+++ scripts/tpu_setup.sh
@@ -1,6 +1,7 @@
 ## conda install
-#curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o Miniconda3-latest-Linux-x86_64.sh
-#bash Miniconda3-latest-Linux-x86_64.sh
+#mkdir downloads
+#curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o ./downloads/Miniconda3-latest-Linux-x86_64.sh
+#bash ./downloads/Miniconda3-latest-Linux-x86_64.sh
 
 ## TODO run installation of v-diffusion-pytorch
 ## move to outer dir
@@ -19,3 +20,6 @@ printf 'export XLA_USE_BF16=1\n' >> ~/.bashrc
 pip install -r ../requirements.txt
 #pip3 install torch_xla[tpuvm] -f https://storage.googleapis.com/tpu-pytorch/wheels/tpuvm/torch_xla-1.10-cp38-cp38-linux_x86_64.whl ## TODO try setting in requrements.txt
 sudo pip3 install https://storage.googleapis.com/cloud-tpu-tpuvm-artifacts/wheels/libtpu-nightly/libtpu_nightly-0.1.dev20211015-py3-none-any.whl
+pip install --upgrade wandb
+pip install --upgrade git+https://github.com/PytorchLightning/pytorch-lightning.git
+
