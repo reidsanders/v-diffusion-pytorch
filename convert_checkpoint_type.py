5d4a7524e064453348bbb5f07819290b0373a452
Reid Sanders <reidsanders@gmail.com>
2022-02-21 13:51:52 -0500

    clean up convert, still not working

--- convert_checkpoint_type.py
+++ convert_checkpoint_type.py
@@ -26,54 +26,15 @@ def main():
     p.add_argument("--outdir", type=str, default="./checkpoints_mod/", help="Directory to save output files to")
     args = p.parse_args()
 
-    model = get_model(args.model)()
-    _, side_y, side_x = model.shape
-    checkpoint = args.checkpoint
-    if not checkpoint:
-        checkpoint = MODULE_DIR / f"checkpoints/{args.model}.pth"
-
-    checkpoint_loaded = torch.load(checkpoint, map_location="cpu")
+    checkpoint_loaded = torch.load(args.checkpoint, map_location="cpu")
     lightning_model = torch.load(target, map_location="cpu")
-    lightning_model[""]
     import ipdb; ipdb.set_trace()
-
 
     state_dict_modified = {
         re.sub("model.(.*)", r"\1", key): value for (key, value) in checkpoint_loaded["state_dict"].items()
     }
 
-    checkpoint_example = MODULE_DIR / f"checkpoints/{args.model}.pth"
-    checkpoint_example_keys = torch.load(checkpoint_example, map_location="cpu").keys()
-    checkpoint_modified = {
-        key: value for (key, value) in checkpoint_modified.items() if key in checkpoint_example_keys
-    }
-    try:
-        model.load_state_dict(checkpoint_modified)
-    except RuntimeError:
-        import ipdb
-        ipdb.set_trace()
-
-    try:
-        model.load_state_dict(checkpoint_loaded)
-    except RuntimeError:
-        print("Runtime error loading state dict, Trying lightning naming schema")
-        checkpoint_modified = {
-            re.sub("model.(.*)", r"\1", key): value for (key, value) in checkpoint_loaded["state_dict"].items()
-        }
-
-        checkpoint_example = MODULE_DIR / f"checkpoints/{args.model}.pth"
-        checkpoint_example_keys = torch.load(checkpoint_example, map_location="cpu").keys()
-        checkpoint_modified = {
-            key: value for (key, value) in checkpoint_modified.items() if key in checkpoint_example_keys
-        }
-        try:
-            model.load_state_dict(checkpoint_modified)
-        except RuntimeError:
-            import ipdb
-            ipdb.set_trace()
-
-    # TODO convert cc12m to lightning schema
-
+    lightning_model["state_dict"] = 
 
 if __name__ == "__main__":
     main()
