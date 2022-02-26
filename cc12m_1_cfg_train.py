b29a5372f4d740d533e561069753143870806bec
Reid Sanders <reid@reidsanders.net>
2022-02-26 16:42:36 -0500

    Simplify get command

--- cc12m_1_cfg_train.py
+++ cc12m_1_cfg_train.py
@@ -3,7 +3,6 @@
 import argparse
 from contextlib import contextmanager
 from copy import deepcopy
-from functools import partial
 import math
 import random
 from pathlib import Path
@@ -74,271 +73,6 @@ def ema_update(model, averaged_model, decay):
 def get_alphas_sigmas(t):
     return torch.cos(t * math.pi / 2), torch.sin(t * math.pi / 2)
 
-
-# Define the model (a residual U-Net)
-
-
-# class ResidualBlock(nn.Module):
-#     def __init__(self, main, skip=None):
-#         super().__init__()
-#         self.main = nn.Sequential(*main)
-#         self.skip = skip if skip else nn.Identity()
-
-#     def forward(self, input):
-#         return self.main(input) + self.skip(input)
-
-
-# class ResLinearBlock(ResidualBlock):
-#     def __init__(self, f_in, f_mid, f_out, is_last=False):
-#         skip = None if f_in == f_out else nn.Linear(f_in, f_out, bias=False)
-#         super().__init__(
-#             [
-#                 nn.Linear(f_in, f_mid),
-#                 nn.ReLU(inplace=True),
-#                 nn.Linear(f_mid, f_out),
-#                 nn.ReLU(inplace=True) if not is_last else nn.Identity(),
-#             ],
-#             skip,
-#         )
-
-
-# class Modulation2d(nn.Module):
-#     def __init__(self, state, feats_in, c_out):
-#         super().__init__()
-#         self.state = state
-#         self.layer = nn.Linear(feats_in, c_out * 2, bias=False)
-
-#     def forward(self, input):
-#         scales, shifts = self.layer(self.state["cond"]).chunk(2, dim=-1)
-#         return torch.addcmul(shifts[..., None, None], input, scales[..., None, None] + 1)
-
-
-# class ResModConvBlock(ResidualBlock):
-#     def __init__(self, state, feats_in, c_in, c_mid, c_out, is_last=False):
-#         skip = None if c_in == c_out else nn.Conv2d(c_in, c_out, 1, bias=False)
-#         super().__init__(
-#             [
-#                 nn.Conv2d(c_in, c_mid, 3, padding=1),
-#                 nn.GroupNorm(1, c_mid, affine=False),
-#                 Modulation2d(state, feats_in, c_mid),
-#                 nn.ReLU(inplace=True),
-#                 nn.Conv2d(c_mid, c_out, 3, padding=1),
-#                 nn.GroupNorm(1, c_out, affine=False) if not is_last else nn.Identity(),
-#                 Modulation2d(state, feats_in, c_out) if not is_last else nn.Identity(),
-#                 nn.ReLU(inplace=True) if not is_last else nn.Identity(),
-#             ],
-#             skip,
-#         )
-
-
-# class SkipBlock(nn.Module):
-#     def __init__(self, main, skip=None):
-#         super().__init__()
-#         self.main = nn.Sequential(*main)
-#         self.skip = skip if skip else nn.Identity()
-
-#     def forward(self, input):
-#         return torch.cat([self.main(input), self.skip(input)], dim=1)
-
-
-# class FourierFeatures(nn.Module):
-#     def __init__(self, in_features, out_features, std=1.0):
-#         super().__init__()
-#         assert out_features % 2 == 0
-#         self.weight = nn.Parameter(torch.randn([out_features // 2, in_features]) * std)
-#         self.weight.requires_grad_(False)
-#         # self.register_buffer('weight', torch.randn([out_features // 2, in_features]) * std)
-
-#     def forward(self, input):
-#         f = 2 * math.pi * input @ self.weight.T
-#         return torch.cat([f.cos(), f.sin()], dim=-1)
-
-
-# class SelfAttention2d(nn.Module):
-#     def __init__(self, c_in, n_head=1, dropout_rate=0.1):
-#         super().__init__()
-#         assert c_in % n_head == 0
-#         self.norm = nn.GroupNorm(1, c_in)
-#         self.n_head = n_head
-#         self.qkv_proj = nn.Conv2d(c_in, c_in * 3, 1)
-#         self.out_proj = nn.Conv2d(c_in, c_in, 1)
-#         self.dropout = nn.Identity()  # nn.Dropout2d(dropout_rate, inplace=True)
-
-#     def forward(self, input):
-#         n, c, h, w = input.shape
-#         qkv = self.qkv_proj(self.norm(input))
-#         qkv = qkv.view([n, self.n_head * 3, c // self.n_head, h * w]).transpose(2, 3)
-#         q, k, v = qkv.chunk(3, dim=1)
-#         scale = k.shape[3] ** -0.25
-#         att = ((q * scale) @ (k.transpose(2, 3) * scale)).softmax(3)
-#         y = (att @ v).transpose(2, 3).contiguous().view([n, c, h, w])
-#         return input + self.dropout(self.out_proj(y))
-
-
-# def expand_to_planes(input, shape):
-#     return input[..., None, None].repeat([1, 1, shape[2], shape[3]])
-
-
-# class DiffusionModel(nn.Module):
-#     def __init__(self):
-#         super().__init__()
-#         c = 128  # The base channel count
-#         cs = [c, c * 2, c * 2, c * 4, c * 4, c * 8, c * 8]
-
-#         self.mapping_timestep_embed = FourierFeatures(1, 128)
-#         self.mapping = nn.Sequential(
-#             ResLinearBlock(512 + 128, 1024, 1024),
-#             ResLinearBlock(1024, 1024, 1024, is_last=True),
-#         )
-
-#         with torch.no_grad():
-#             for param in self.mapping.parameters():
-#                 param *= 0.5**0.5
-
-#         self.state = {}
-#         conv_block = partial(ResModConvBlock, self.state, 1024)
-
-#         self.timestep_embed = FourierFeatures(1, 16)
-#         self.down = nn.AvgPool2d(2)
-#         self.up = nn.Upsample(
-#             scale_factor=2,
-#             mode="nearest",
-#             # align_corners=False
-#         )
-
-#         self.net = nn.Sequential(  # 256x256
-#             conv_block(3 + 16, cs[0], cs[0]),
-#             conv_block(cs[0], cs[0], cs[0]),
-#             conv_block(cs[0], cs[0], cs[0]),
-#             conv_block(cs[0], cs[0], cs[0]),
-#             SkipBlock(
-#                 [
-#                     self.down,  # 128x128
-#                     conv_block(cs[0], cs[1], cs[1]),
-#                     conv_block(cs[1], cs[1], cs[1]),
-#                     conv_block(cs[1], cs[1], cs[1]),
-#                     conv_block(cs[1], cs[1], cs[1]),
-#                     SkipBlock(
-#                         [
-#                             self.down,  # 64x64
-#                             conv_block(cs[1], cs[2], cs[2]),
-#                             conv_block(cs[2], cs[2], cs[2]),
-#                             conv_block(cs[2], cs[2], cs[2]),
-#                             conv_block(cs[2], cs[2], cs[2]),
-#                             SkipBlock(
-#                                 [
-#                                     self.down,  # 32x32
-#                                     conv_block(cs[2], cs[3], cs[3]),
-#                                     conv_block(cs[3], cs[3], cs[3]),
-#                                     conv_block(cs[3], cs[3], cs[3]),
-#                                     conv_block(cs[3], cs[3], cs[3]),
-#                                     SkipBlock(
-#                                         [
-#                                             self.down,  # 16x16
-#                                             conv_block(cs[3], cs[4], cs[4]),
-#                                             SelfAttention2d(cs[4], cs[4] // 64),
-#                                             conv_block(cs[4], cs[4], cs[4]),
-#                                             SelfAttention2d(cs[4], cs[4] // 64),
-#                                             conv_block(cs[4], cs[4], cs[4]),
-#                                             SelfAttention2d(cs[4], cs[4] // 64),
-#                                             conv_block(cs[4], cs[4], cs[4]),
-#                                             SelfAttention2d(cs[4], cs[4] // 64),
-#                                             SkipBlock(
-#                                                 [
-#                                                     self.down,  # 8x8
-#                                                     conv_block(cs[4], cs[5], cs[5]),
-#                                                     SelfAttention2d(cs[5], cs[5] // 64),
-#                                                     conv_block(cs[5], cs[5], cs[5]),
-#                                                     SelfAttention2d(cs[5], cs[5] // 64),
-#                                                     conv_block(cs[5], cs[5], cs[5]),
-#                                                     SelfAttention2d(cs[5], cs[5] // 64),
-#                                                     conv_block(cs[5], cs[5], cs[5]),
-#                                                     SelfAttention2d(cs[5], cs[5] // 64),
-#                                                     SkipBlock(
-#                                                         [
-#                                                             self.down,  # 4x4
-#                                                             conv_block(cs[5], cs[6], cs[6]),
-#                                                             SelfAttention2d(cs[6], cs[6] // 64),
-#                                                             conv_block(cs[6], cs[6], cs[6]),
-#                                                             SelfAttention2d(cs[6], cs[6] // 64),
-#                                                             conv_block(cs[6], cs[6], cs[6]),
-#                                                             SelfAttention2d(cs[6], cs[6] // 64),
-#                                                             conv_block(cs[6], cs[6], cs[6]),
-#                                                             SelfAttention2d(cs[6], cs[6] // 64),
-#                                                             conv_block(cs[6], cs[6], cs[6]),
-#                                                             SelfAttention2d(cs[6], cs[6] // 64),
-#                                                             conv_block(cs[6], cs[6], cs[6]),
-#                                                             SelfAttention2d(cs[6], cs[6] // 64),
-#                                                             conv_block(cs[6], cs[6], cs[6]),
-#                                                             SelfAttention2d(cs[6], cs[6] // 64),
-#                                                             conv_block(cs[6], cs[6], cs[5]),
-#                                                             SelfAttention2d(cs[5], cs[5] // 64),
-#                                                             self.up,
-#                                                         ]
-#                                                     ),
-#                                                     conv_block(cs[5] * 2, cs[5], cs[5]),
-#                                                     SelfAttention2d(cs[5], cs[5] // 64),
-#                                                     conv_block(cs[5], cs[5], cs[5]),
-#                                                     SelfAttention2d(cs[5], cs[5] // 64),
-#                                                     conv_block(cs[5], cs[5], cs[5]),
-#                                                     SelfAttention2d(cs[5], cs[5] // 64),
-#                                                     conv_block(cs[5], cs[5], cs[4]),
-#                                                     SelfAttention2d(cs[4], cs[4] // 64),
-#                                                     self.up,
-#                                                 ]
-#                                             ),
-#                                             conv_block(cs[4] * 2, cs[4], cs[4]),
-#                                             SelfAttention2d(cs[4], cs[4] // 64),
-#                                             conv_block(cs[4], cs[4], cs[4]),
-#                                             SelfAttention2d(cs[4], cs[4] // 64),
-#                                             conv_block(cs[4], cs[4], cs[4]),
-#                                             SelfAttention2d(cs[4], cs[4] // 64),
-#                                             conv_block(cs[4], cs[4], cs[3]),
-#                                             SelfAttention2d(cs[3], cs[3] // 64),
-#                                             self.up,
-#                                         ]
-#                                     ),
-#                                     conv_block(cs[3] * 2, cs[3], cs[3]),
-#                                     conv_block(cs[3], cs[3], cs[3]),
-#                                     conv_block(cs[3], cs[3], cs[3]),
-#                                     conv_block(cs[3], cs[3], cs[2]),
-#                                     self.up,
-#                                 ]
-#                             ),
-#                             conv_block(cs[2] * 2, cs[2], cs[2]),
-#                             conv_block(cs[2], cs[2], cs[2]),
-#                             conv_block(cs[2], cs[2], cs[2]),
-#                             conv_block(cs[2], cs[2], cs[1]),
-#                             self.up,
-#                         ]
-#                     ),
-#                     conv_block(cs[1] * 2, cs[1], cs[1]),
-#                     conv_block(cs[1], cs[1], cs[1]),
-#                     conv_block(cs[1], cs[1], cs[1]),
-#                     conv_block(cs[1], cs[1], cs[0]),
-#                     self.up,
-#                 ]
-#             ),
-#             conv_block(cs[0] * 2, cs[0], cs[0]),
-#             conv_block(cs[0], cs[0], cs[0]),
-#             conv_block(cs[0], cs[0], cs[0]),
-#             conv_block(cs[0], cs[0], 3, is_last=True),
-#         )
-
-#         with torch.no_grad():
-#             for param in self.net.parameters():
-#                 param *= 0.5**0.5
-
-#     def forward(self, input, t, clip_embed):
-#         clip_embed = F.normalize(clip_embed, dim=-1) * clip_embed.shape[-1] ** 0.5
-#         mapping_timestep_embed = self.mapping_timestep_embed(t[:, None])
-#         self.state["cond"] = self.mapping(torch.cat([clip_embed, mapping_timestep_embed], dim=1))
-#         timestep_embed = expand_to_planes(self.timestep_embed(t[:, None]), input.shape)
-#         out = self.net(torch.cat([input, timestep_embed], dim=1))
-#         self.state.clear()
-#         return out
-
-
 @torch.no_grad()
 def cfg_sample(model, steps, eta, method="ddim", batchsize=1):
     """Draws samples from a model given starting noise."""
@@ -347,28 +81,6 @@ def cfg_sample(model, steps, eta, method="ddim", batchsize=1):
 
     zero_embed = torch.zeros([1, model.clip_model.visual.output_dim], device=model.device)
     target_embeds, weights = [zero_embed], []
-
-    # normalize = transforms.Normalize(
-    #     mean=[0.48145466, 0.4578275, 0.40821073],
-    #     std=[0.26862954, 0.26130258, 0.27577711],
-    # )
-
-    # for prompt in args.prompts:
-    #     txt, weight = parse_prompt(prompt)
-    #     target_embeds.append(clip_model.encode_text(clip.tokenize(txt).to(device)).float())
-    #     weights.append(weight)
-
-    # for prompt in args.images:
-    #     path, weight = parse_prompt(prompt)
-    #     img = Image.open(diffusionutils.fetch(path)).convert("RGB")
-    #     clip_size = clip_model.visual.input_resolution
-    #     img = resize_and_center_crop(img, (clip_size, clip_size))
-    #     batch = TF.to_tensor(img)[None].to(device)
-    #     embed = F.normalize(clip_model.encode_image(normalize(batch)).float(), dim=-1)
-    #     target_embeds.append(embed)
-    #     weights.append(weight)
-
-    # torch.manual_seed(args.seed)
 
     def cfg_model_fn(x, t):
         n = x.shape[0]
@@ -398,8 +110,6 @@ def cfg_sample(model, steps, eta, method="ddim", batchsize=1):
         for i in trange(0, n, batchsize):
             cur_batch_size = min(n - i, batchsize)
             outs = run(x[i : i + cur_batch_size], steps)
-            # for j, out in enumerate(outs):
-            #     img = diffusionutils.to_pil_image(out)
         return outs
 
     weights = torch.tensor([1 - sum(weights), *weights], device=model.device)
@@ -631,7 +341,6 @@ class DemoCallback(pl.Callback):
         log_dict = {
             "demo_grid": wandb.Image(image),
             "prompts": wandb.Html(f"<pre>{lines_text}</pre>"),
-            #'metrics_report': wandb.Html(f'<pre>{metrics_report}</pre>')
         }
         trainer.logger.experiment.log(log_dict, step=trainer.global_step)
 
@@ -658,43 +367,13 @@ def worker_init_fn(worker_id):
     random.seed(torch.initial_seed())
 
 
-def get_orig_cmd(max_width=80, full_python_path=False):
-    """
-    form
-    Return the original command line string that can be replayed
-    nicely and wrapped for 80 char width
-    Args:
-        - max_width: the width to wrap for. defaults to 80
-        - full_python_path: whether to replicate the full path
-          or just the last part (i.e. `python`). default to `False`
-    """
-
+def get_command_as_called():
     cmd = []
-
-    # deal with critical env vars
-    # env_keys = ["CUDA_VISIBLE_DEVICES"]
-    env_keys = []
-    for key in env_keys:
-        val = os.environ.get(key, None)
-        if val is not None:
-            cmd.append(f"{key}={val}")
-
-    # python executable (not always needed if the script is executable)
-    python = sys.executable if full_python_path else sys.executable.split("/")[-1]
+    python = sys.executable.split("/")[-1]
     cmd.append(python)
-
-    # now the normal args
+    # add args
     cmd += list(map(shlex.quote, sys.argv))
-
-    # split up into up to MAX_WIDTH lines with shell multi-line escapes
-    lines = []
-    current_line = ""
-    while len(cmd) > 0:
-        current_line += f"{cmd.pop(0)} "
-        if len(cmd) == 0 or len(current_line) + len(cmd[0]) + 1 > max_width - 1:
-            lines.append(current_line)
-            current_line = ""
-    return " ".join(lines)
+    return " ".join(cmd)
 
 
 def rename_lightning_checkpoint_keys(checkpoint, lightning_state_dict):
@@ -933,7 +612,6 @@ def main():
 if __name__ == "__main__":
     # Fix crashes on multiple tpu cores, but breaks stdout logging
     ### See https://github.com/wandb/client/issues/1994
-    # os.environ['WANDB_CONSOLE'] = 'off'
     wandb.require(experiment="service")
     wandb.setup()
     main()
