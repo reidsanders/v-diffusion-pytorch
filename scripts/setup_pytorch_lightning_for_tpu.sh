419ba5d444806c20c453f1da4ba08de52550fc65
Reid Sanders <reid@reidsanders.net>
2022-02-26 17:23:27 -0500

    simplified requirements, moved scripts out

--- scripts/setup_pytorch_lightning_for_tpu.sh
+++
@@ -1,4 +0,0 @@
-curl https://raw.githubusercontent.com/pytorch/xla/master/contrib/scripts/env-setup.py -o pytorch-xla-env-setup.py > /dev/null
-python3 pytorch-xla-env-setup.py --version nightly --apt-packages libomp5 libopenblas-dev > /dev/null
-pip3 install pytorch-lightning > /dev/null
-
